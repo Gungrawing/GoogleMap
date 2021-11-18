6e5a47e128c8f6b3411e9fe8f0dcfb6ce79d23e4
John <thesaikon@gmail.com>
2021-11-18 12:34:47 +0200

    last version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import {
+    GoogleMap,
+    useLoadScript,
+    Marker,
+    InfoWindow,
+} from '@react-google-maps/api';
+// import {formatRelative} from "data-fns";
+import "@reach/combobox/styles.css"
+import mapStyle from "./components/style-map/style-map"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const libraries = ['places'];
+
+
+
+export default function App() {
+    const [isLat, setLat] = useState('')
+    const [isLng, setLng] = useState('')
+    const [isZoom,setZoom]= useState(8)
+    const [center,setCenter]= useState({
+        lat: 40.713051,
+        lng: -74.007233,
+    })
+    const mapContainerStyle = {
+        width: '100vw',
+        height: '100vh'
+    }
+    const options = {
+        style: mapStyle
+    }
+    useEffect(() => {
+        const handleUserKeyPress = event => {
+            const { key, keyCode } = event;
+            if (keyCode === 32 || (keyCode >= 65 && keyCode <= 90)) {
+                setZoom(isZoom => `${isZoom}${key}`);
+                event.preventDefault()
+            }
+
+        };
+        window.addEventListener("keydown", handleUserKeyPress);
+        return () => {
+            window.removeEventListener("keydown", handleUserKeyPress);
+        };
+    }, );
+
+
+    function getCenter(){
+        setCenter({
+            lat:+isLat,
+            lng:+isLng,
+            handleUserKeyPress: +isZoom,
+        })
+
+    }
+    const {isLoaded,loadError} = useLoadScript(
+        {
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEYS="AIzaSyBy_R5G2M6bR5H-ZDeeQNZQzMJx4Xuk4pY",
+        libraries,
+    });
+    if(loadError) return "Error Loading maps";
+    if(!isLoaded) return "Loading maps";
+
+    return (
+<div>
+    <h6>
+        <div>
+            <label>Set Latitude</label>
+            <input type="text"
+                   required
+                   value={isLat}
+                   onChange={(e)=>setLat(e.target.value)}/>
+            <label>Set Longitude</label>
+            <input type="text"
+                   required
+                   value={isLng}
+                   onChange={(e)=>setLng(e.target.value)}/>
+            <input type="number"
+                   required
+                   onKeyDown={(e)=>setZoom(+e.target.value)}/>
+        </div>
+        <button onClick={getCenter}>Click To Change Center</button>
+    </h6>
+    <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={isZoom}
+        center={center}
+        options={options}>
+    </GoogleMap>
+</div>
+    );
 }
-
-export default App;
+// <input type="number"
+//        required
+//        onChange={(e)=>{window.addEventListener('keypress',()=>{
+//            setZoom(+e.target.value)
+//        })}}/>
